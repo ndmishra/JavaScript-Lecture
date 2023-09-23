12d6e56a9d6a81583d35817d00e7fb37ed0b2c49
ndmishra <92421199+ndmishra@users.noreply.github.com>
2023-09-23 19:08:04 +0000

    Conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let num ="33adfas"
+
+let convertstring = Number(num)
+//console.log(convertstring);
+//console.log(typeof convertstring);
+
+let IsLoggedIn =1
+
+let ConvertIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(ConvertIsLoggedIn)
+
+//"33" =33
+//"33KJADL" = NaN
+// 1=>True  and 0=>false
