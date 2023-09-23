5af3c86853f8d06db3dfa8738d3daa4e362bf120
ndmishra <92421199+ndmishra@users.noreply.github.com>
2023-09-23 18:43:48 +0000

    Variables and DataTypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,21 @@
+const AccountId =255585
+let AcountMailId ="ndmishra.google.com"
+var AccountNumber ='525558'
+AccountCity ="Lucknow"
+let AccountState ;
+
+//AccountId=2555
+console.log(AccountId);
+
+AccountMailId ="ndmishra@gmail.com"
+
+AccountNumber="256"
+
+AccountCity ="Delhi"
+
+
+/*
+Prefer not to use var Because of Issue of Block Scope
+
+*/
+console.table([AccountId,AccountMailId,AccountNumber,AccountCity,AccountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let age=25
+let name ="Narayan"
+let IsLoggedIn = true
+let state =null
+let country;
+
+
+// number =>2 to Power 53
+//bigint
+//string =""
+//boolean =true/false 
+// null = standalone value
+//Undefined
+// symbol = Unique
+
+//object
+
+console.log(typeof true)
+
